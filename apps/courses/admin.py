d17f4ea5c754b3ccca37acafedb5c2c03dcde7fa
Ivan.Kataev <Ivan.Kataev@x5.ru>
2020-04-19 16:36:47 +0300

    В приложение courses добавлена модель участника курсов
    Signed-off-by: Ivan.Kataev <Ivan.Kataev@x5.ru>

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, CourseParticipant
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ["id", "name"]
     search_fields = ["name"]
+
+
+@admin.register(CourseParticipant)
+class CourseParticipantAdmin(admin.ModelAdmin):
+    list_display = ["id", "course", "student"]
--- apps/courses/models.py
+++ apps/courses/models.py
@@ -36,3 +36,29 @@ class Course(TimeStampedModel):
 
     def __str__(self):
         return self.name[:80]
+
+
+class CourseParticipant(TimeStampedModel):
+    course = models.ForeignKey(
+        'courses.Course',
+        related_name='participants_set',
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    student = models.ForeignKey(
+        'students.Student',
+        related_name='courses_set',
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+    is_completed = models.BooleanField(
+        _("is completed"),
+        default=False
+    )
+
+    class Meta:
+        db_table = 'course_participants'
+        unique_together = ["course", "student"]
+
+    def __str__(self):
+        return f"{self.student} participate in {self.course}"
